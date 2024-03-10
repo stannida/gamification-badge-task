3ca9fb00ed6dd8faa329e688fdfc2309af283090
Aleksandra Stankevich <aleksandra.stankevich@talentsconnect.com>
2024-03-10 16:38:38 +0100

    adding badge analysis to the dashboard and removing helping functions to tools file

--- dashboard.py
+++ dashboard.py
@@ -6,6 +6,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
+from tools import *
 
 #streamlit global parameters and layout
 st.set_page_config(layout="wide", page_title="Badge Criteria Analysis")
@@ -13,25 +14,23 @@ st.title("Badge Criteria Analysis")
 left_empty_col, main_body, right_column = st.columns([2, 15, 5])
 
 #read data
-data = pd.read_csv("resources/Technical Challenge Data Analyst.csv")
-data.fillna(0, inplace=True)
-data = data.astype('int64')
-data['USER_ID'] = data['USER_ID'].astype(str)
-event_columns = ['POSTS_CREATED','REPLIES_RECEIVED','THANKYOUS_RECEIVED','EVENTS_CREATED','EVENT_PARTICIPANTS','ITEMS_GIFTED','PLACES_RECOMMENDED']
-# data['active'] = (data[event_columns] != 0).any(axis=1)
+data = read_data()
 
-#dataframe with distribution of events per each event type
-# distribution_events_df = data.groupby('LAST_X_WEEKS').describe().drop(columns=['USER_ID']).transpose()
-# distribution_events_df = data.set_index('LAST_X_WEEKS').stack().reset_index().rename(columns={'level_1': 'groups', 0: 'values'})
+#action columns names
+action_columns = ['POSTS_CREATED','REPLIES_RECEIVED','THANKYOUS_RECEIVED','EVENTS_CREATED','EVENT_PARTICIPANTS','ITEMS_GIFTED','PLACES_RECOMMENDED']
+
+#count the percentage of users with non-zero actions per week, out of total
+users_per_action_type = get_users_per_action(data, action_columns)
+
 
 with main_body:
     st.markdown('''
-This is an interactive dashboard that helps to analyse user participation on the platform. The goal of this dashboard is to understand the user distribution for each of the following events:
+This is an interactive dashboard that helps to analyse user participation on the platform. The goal of this dashboard is to understand the user distribution for each of the following actions:
 - Posts_created: Number of posts the user created in given time frame
 - Replies_received: Number of replies to posts the user received in given time frame
 - Thankyous_received: Number of thankyou reactions the user received in given time frame
-- Events_created: Number of events created by user in given time frame
-- Event_participants: Number of users that attended events by user in given time frame
+- Events_created: Number of actions created by user in given time frame
+- Event_participants: Number of users that attended actions by user in given time frame
 - Items_gifted: Number of items the user gave away for free in given time frame
 - Places_recommended: Number of places the user recommended in given time frame
                 
@@ -42,35 +41,37 @@ You can use the following widgets to select a subset of data for exploration. Th
         'Choose the number of weeks',
         data['LAST_X_WEEKS'].unique(),
         help='The timeframe that you want to include data for (up to 4, 6, 8 or 12 weeks)')
-    event_select = st.selectbox(
-        'Choose the event',
-        ['All']+event_columns,
-        help='The event type (all or one of the above)')
+    action_select = st.selectbox(
+        'Choose the action',
+        ['All']+action_columns,
+        help='The action type (all or one of the above)')
     not_null_select = st.radio(
         'Should non-active users be included?',
         [True, False],
-        help='Whether to include non-active users. For allevent types  a non-active user is someone who had none of the events in the selected timeframe. For one selected event only non-zero entries are included.')
+        help='Whether to include non-active users. For all action types  a non-active user is someone who had none of the actions in the selected timeframe. For one selected action only non-zero entries are included.')
     
-
+    #filter the dataset for the selected week
     week_select_df = data[data['LAST_X_WEEKS']==week_select]
-    if event_select!='All':
-        week_event_select_df = week_select_df[event_select].reset_index()
-        dist_columns = [event_select]
-        # week_event_select_df['active'] = (week_event_select_df != 0)
+
+    #if all actions are selected the full dataset with only action columns is returned
+    if action_select!='All':
+        week_action_select_df = week_select_df[action_select].reset_index()
+        dist_columns = [action_select]
+        #if users with only non zero actions are selected
         if not not_null_select:
-            week_event_select_df = week_event_select_df[week_event_select_df!=0]
+            week_action_select_df = week_action_select_df[week_action_select_df!=0]
     else:
-        week_event_select_df = week_select_df[event_columns]
-        dist_columns = event_columns
+        week_action_select_df = week_select_df[action_columns]
+        dist_columns = action_columns
         if not not_null_select:
-            week_event_select_df = week_event_select_df[(week_event_select_df[dist_columns] != 0).any(axis=1)]
+            week_action_select_df = week_action_select_df[(week_action_select_df[dist_columns] != 0).any(axis=1)]
 
-    # st.write(week_event_select_df)
-
+    st.write(f"Distribution(s) of the number of actions performed by users in the last {week_select} weeks:")
+    #plotly chart with boxplots per all actions or just one selected
     fig = make_subplots(rows=1, cols=len(dist_columns))
     for i, var in enumerate(dist_columns):
         fig.add_trace(
-            go.Box(y=week_event_select_df[var],
+            go.Box(y=week_action_select_df[var],
             name=var),
             row=1, col=i+1
         )
@@ -78,40 +79,105 @@ You can use the following widgets to select a subset of data for exploration. Th
     fig.update_traces(boxpoints=False,jitter=.3)
     st.plotly_chart(fig,use_container_width=True)
 
+    #dataframe table with distribution of users per action per week
+    st.write("Percentage of users with at least 1 action of each type out of total number of users in the last N weeks.")
+    st.dataframe(users_per_action_type)
+
     st.divider()
+
+    #specific badges
     st.header("Badges")
-    st.subheader("Event Planner")
-    pct_users_badge = np.round(data[data['EVENTS_CREATED']>0]['USER_ID'].nunique()*100/data['USER_ID'].nunique(),1)
-    badge_df_per_week = data[data['EVENTS_CREATED']>0].groupby('LAST_X_WEEKS').agg({'EVENTS_CREATED':'mean','EVENT_PARTICIPANTS':'mean','USER_ID':'nunique'})
-    badge_df_per_week['% of all users'] = badge_df_per_week['USER_ID']*100/data['USER_ID'].nunique()
-    badge_df_per_week.rename(columns={'EVENTS_CREATED':"Avg. Events created per user",
-                                      'EVENT_PARTICIPANTS':"Avg. number of participants",
-                                      'USER_ID':'Number of users who created events'},inplace=True)
-
-    st.markdown(f'''This badge is given to users that create events with a certain number of participants in given timeframe.  
-{pct_users_badge}% of all users created at least 1 event in the last 12 weeks.
-                ''')
-    st.dataframe(badge_df_per_week)
-
     col1,col2,col3 = st.columns(3)
-    user_pct_filter = col1.slider(label="% of users to award the badge to", 
+    badge_select = col1.selectbox(
+                                'Select the badge type',
+                                ('Event Planner', 'Conversation Starter', 'Philanthropist','Helping Hand','Local Guide'))
+    user_pct_filter = col2.slider(label="top % of users to award the badge to", 
                                   min_value=1,
                                   max_value=50,
-                                  value=20)
-    week_filter = col2.selectbox(
+                                  value=20,
+                                  help='Where the threshold for the badge achievement lies if all users are ranked by the number of events they created')
+    week_filter = col3.selectbox(
         'Choose the number of weeks',
-        badge_df_per_week.index)
+        data['LAST_X_WEEKS'].unique())
+    
+    #selected badge type
+    st.subheader(badge_select)
 
-    # fig = px.histogram(data[data['EVENTS_CREATED']>0], x="EVENTS_CREATED", color="LAST_X_WEEKS")
-    # st.plotly_chart(fig,use_container_width=True)
+    #placeholders for the text and visualisation for each selected badge
+    if badge_select=='Event Planner':
+        main_action_column, second_action_column, second_column_limit = 'EVENTS_CREATED', 'EVENT_PARTICIPANTS', 1
+        badge_data = data
+        badge_description = 'This badge is given to users that create events with a certain number of participants in given timeframe'
+        most_common_action = 'created only 1 event with 1 participant out of all users who created events'
+        action_metric_name = 'participants'
+        action_verb = 'create'
+    elif badge_select=='Conversation Starter':
+        main_action_column, second_action_column, second_column_limit = 'POSTS_CREATED', 'REPLIES_RECEIVED', 0
+        badge_data = data[data['USER_ID']!='17211']
+        badge_description = 'This badge is given to users that create a certain amount of posts with replies in given timeframe'
+        most_common_action = 'created only 1 post with 0 replies out of all users who posted'
+        action_metric_name = 'replies'
+        action_verb = 'create'
+    elif badge_select=='Philanthropist':
+        main_action_column, second_action_column, second_column_limit = 'ITEMS_GIFTED', None, 0
+        badge_data = data
+        badge_description = 'This badge is given to users that give away items for free in given timeframe'
+        most_common_action = 'gifted only 1 item of all users who gifted on the marketplace'
+        action_verb = 'give away'
+    elif badge_select=='Helping Hand':
+        main_action_column, second_action_column, second_column_limit = 'THANKYOUS_RECEIVED', None, 0
+        badge_data = data
+        badge_description = 'This badge is given to users that receive thank you messages in given timeframe'
+        most_common_action = 'received only 1 thank-you message of all users who received any'
+        action_verb = 'receive'
+    elif badge_select=='Local Guide':
+        main_action_column, second_action_column, second_column_limit = 'PLACES_RECOMMENDED', None, 0
+        badge_data = data
+        badge_description = 'This badge is given to users that recommend places in given timeframe'
+        most_common_action = 'recommended only 1 place of all users who recommended any'
+        action_verb = 'recommend'
 
-    #filter out for events more than 1 and number of perticipants not 0
-    event_df = data[(data['EVENTS_CREATED']>0)&(data['LAST_X_WEEKS']==week_filter)&(data['EVENT_PARTICIPANTS']>0)].sort_values(by = 'EVENTS_CREATED', ascending = False)
-    event_df['Events created, cummulative'] = event_df['EVENTS_CREATED'].cumsum()
-    event_df['%of events created, cummulative'] = event_df['Events created, cummulative']/event_df['EVENTS_CREATED'].sum()
-    event_df['Avg. participants/event'] = np.round(event_df['EVENT_PARTICIPANTS']/event_df['EVENTS_CREATED'])
-    event_df = event_df[['EVENTS_CREATED','Avg. participants/event','USER_ID','Events created, cummulative','%of events created, cummulative']]
-    event_df_top_pct = event_df.head(int(len(event_df)*(user_pct_filter/100))).reset_index(drop=True)
-    st.dataframe(event_df_top_pct)
-    st.markdown(f"For top {user_pct_filter}% of users to earn the badge they need to create at least {event_df_top_pct['EVENTS_CREATED'].min()} events with {event_df_top_pct['Avg. participants/event'].min()} participants.")
+    #get the dataframes with weekly distribution of users per action as well as a ranked dataframe by the number of actions performed
+    pct_users_with_action, pct_users_with_one_action, action_df_per_week, action_df_cumm = get_action_stats(badge_data, week_filter, main_action_column, second_action_column, second_column_limit)
+
+    st.markdown(f'''{badge_description}.  
+**{pct_users_with_action}%** of all users created at least 1 action in the last 12 weeks.  
+**{pct_users_with_one_action}%** of all users {most_common_action} in the last 12 weeks.
+                ''')
+    #avg. number of actions per user, total number of users who did the action and a percentage of active users out of all per each week
+    st.dataframe(action_df_per_week)
+
+    #top X% of users who performed this action, the X is selected in the slider filter
+    action_df_top_pct = action_df_cumm.head(int(len(action_df_cumm)*(user_pct_filter/100))).reset_index(drop=True)
+    st.dataframe(action_df_top_pct)
+
+    #relationship between first and second action columns (e.g. events and event participants), colored by the number of users who created such events
+    if second_action_column:
+        participants_per_action_freq = badge_data[(badge_data[main_action_column]>0)&(badge_data['LAST_X_WEEKS']==week_filter)].groupby([main_action_column, second_action_column+'_AVG','LAST_X_WEEKS'])['USER_ID'].nunique().reset_index()
+        participants_per_action_freq.rename(columns={"USER_ID":"Number of Users"},inplace=True)
+        fig = px.scatter(participants_per_action_freq, x=main_action_column, y=second_action_column+'_AVG',
+                 color='Number of Users')
+    else: #only the number of events matters, the chart is a simple distribution of users per number of actions
+        participants_per_action_freq = badge_data[(badge_data[main_action_column]>0)&(badge_data['LAST_X_WEEKS']==week_filter)].groupby([main_action_column,'LAST_X_WEEKS'])['USER_ID'].nunique().reset_index()
+        participants_per_action_freq.rename(columns={"USER_ID":"Number of Users"},inplace=True)
+        fig = px.scatter(participants_per_action_freq, x=main_action_column, y='Number of Users')
+
+    st.plotly_chart(fig,use_container_width=True,theme=None)
+
+    #percentage of total users that will receive a badge
+    badge_achievers_pct = np.round(action_df_cumm[action_df_cumm[main_action_column]>=action_df_top_pct[main_action_column].min()]['USER_ID'].nunique()*100/badge_data[badge_data['LAST_X_WEEKS']==week_filter]['USER_ID'].nunique(),1)
+
+    if second_action_column:
+        st.markdown(f'''For top {user_pct_filter}% of users to earn the badge they need to {action_verb} at least {action_df_top_pct[main_action_column].min()} actions with {action_df_top_pct[second_action_column+'_AVG'].min()} {action_metric_name} on average in {week_filter} weeks.  
+    This way the bagde would be awarded to {badge_achievers_pct}% of all users.
+                ''')
+    else:
+        st.markdown(f'''For top {user_pct_filter}% of users to earn the badge they need to {action_verb} at least {action_df_top_pct[main_action_column].min()} actions in {week_filter} weeks.  
+    This way the bagde would be awarded to {badge_achievers_pct}% of all users.
+                ''')
+    st.divider()
+
+
+
+
 
---
+++ tools.py
@@ -0,0 +1,89 @@
+import pandas as pd
+import numpy as np
+
+def read_data():
+    '''
+    Read the dataset and clean it: fill null values, convert to the right format and introduce missing columns
+    '''
+
+    data = pd.read_csv("resources/Technical Challenge Data Analyst.csv")
+    data.fillna(0, inplace=True)
+
+    #convert all columns but user IDs to integer
+    data = data.astype('int64')
+    data['USER_ID'] = data['USER_ID'].astype(str)
+
+    #New column with average number of participants per event
+    data['AVG_PARTICIPANTS_action'] = np.round(data['EVENT_PARTICIPANTS']/data['EVENTS_CREATED'])
+
+    return data
+
+def get_users_per_action(data, action_columns):
+    '''
+    Count the percentage of users with non-zero actions per week, out of total
+    Args:
+        data: dataframe
+        action_columns: list of column names
+
+    Returns:
+        users_per_action_type: dataframe with number and percentage of users per action and week, sorted by week and % of users in descending order
+    '''
+    users_per_action_type = pd.DataFrame()
+    for action_type in action_columns:
+        non_empty_action_data = data[data[action_type]>0]
+        users_for_each_action = non_empty_action_data.groupby(['LAST_X_WEEKS'])['USER_ID'].nunique().reset_index()
+        users_for_each_action['ACTION_TYPE'] = action_type
+        users_per_action_type = pd.concat([users_per_action_type, users_for_each_action])
+        
+
+    users_per_action_type = users_per_action_type.merge(data.groupby('LAST_X_WEEKS').agg('USER_ID').nunique().reset_index(),how='left',on='LAST_X_WEEKS',suffixes=('_ACTION','_TOTAL'))
+    users_per_action_type['%_OF_TOTAL_USERS'] = users_per_action_type['USER_ID_ACTION']*100/users_per_action_type['USER_ID_TOTAL']
+
+    return users_per_action_type[['LAST_X_WEEKS','ACTION_TYPE','USER_ID_ACTION','%_OF_TOTAL_USERS']].sort_values(['LAST_X_WEEKS','%_OF_TOTAL_USERS'],ascending=[False,True])
+
+def get_action_stats(data, week_filter, main_action_column, second_action_column, second_column_limit):
+    '''
+    Get dataframes for the selected action types and percentages of active users.
+    Args:
+        data: dataframe, users and actions dataframe
+        week_filter: int, filter for the last X weeks
+        main_action_column: string, name for the action type column
+        second_action_column: string, name for the action metric column, like participants or replies. None if not applicable
+        second_column_limit: integer, most common value for the action metric column. 0 if not applicable
+
+    Returns:
+        pct_users_with_action: float, percentage of users who have done at least 1 action out of all users 
+        pct_users_with_one_action: float, percentage of users who have done exctly 1 action (and 0 or 1 participants/replies) out of all users who have done this action
+        action_df_per_week: dataframe, avg. number of actions per user, avg. number of participants/replies per action, number of users who have done this action and percentage of total PER EACH WEEK PERIOD
+        action_df_cumm: dataframe, data for each user who performed an action with cummulative number and percentage for action number and ranked by the number of actions
+    '''
+    pct_users_with_action = np.round(data[data[main_action_column]>0]['USER_ID'].nunique()*100/data['USER_ID'].nunique(),1)
+    if second_action_column:
+        pct_users_with_one_action = np.round(data[(data[main_action_column]==1)&(data[second_action_column]==second_column_limit)]['USER_ID'].nunique()*100/data[(data[main_action_column]>0)]['USER_ID'].nunique(),1)
+        action_df_per_week = data[data[main_action_column]>0].groupby('LAST_X_WEEKS').agg({main_action_column:'mean',second_action_column:'mean','USER_ID':'nunique'})
+        action_df_per_week.rename(columns={main_action_column:"Avg. action per user",
+                                      second_action_column:"Avg. number of participants/replies",
+                                      'USER_ID':'Number of users'},inplace=True)
+        
+        data[second_action_column+'_AVG'] = data[second_action_column]/data[main_action_column]
+        data[second_action_column+'_AVG'] = data[second_action_column+'_AVG'].round()
+        action_df_cumm = data[(data[main_action_column]>0)&(data['LAST_X_WEEKS']==week_filter)&(data[second_action_column+'_AVG']>0)].sort_values(by=main_action_column, ascending=False)
+
+        action_df_cumm = action_df_cumm[[main_action_column,second_action_column+'_AVG','USER_ID']]
+    else:
+        pct_users_with_one_action = np.round(data[(data[main_action_column]==1)]['USER_ID'].nunique()*100/data[(data[main_action_column]>0)]['USER_ID'].nunique(),1)
+        action_df_per_week = data[data[main_action_column]>0].groupby('LAST_X_WEEKS').agg({main_action_column:'mean','USER_ID':'nunique'})
+        action_df_per_week.rename(columns={main_action_column:"Avg. action per user",
+                                      'USER_ID':'Number of users'},inplace=True)
+        
+        action_df_cumm = data[(data[main_action_column]>0)&(data['LAST_X_WEEKS']==week_filter)].sort_values(by=main_action_column, ascending=False)
+        action_df_cumm = action_df_cumm[[main_action_column,'USER_ID']]
+
+    
+    action_df_per_week['% of all users'] = action_df_per_week['Number of users']*100/data['USER_ID'].nunique()
+
+
+    action_df_cumm['TOTAL_ACTIONS_CUMMULATIVE'] = action_df_cumm[main_action_column].cumsum()
+    action_df_cumm['%_OF_ACTIONS_CUMMULATIVE'] = action_df_cumm['TOTAL_ACTIONS_CUMMULATIVE']/action_df_cumm[main_action_column].sum()
+
+    return pct_users_with_action, pct_users_with_one_action, action_df_per_week, action_df_cumm
